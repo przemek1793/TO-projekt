eec5d4dfa66fb150efa36c5b178166cf9c6c9049
Przemysław Pajestkaw <przemek1793@gmail.com>
2018-01-03 21:40:49 +0100

    Logout

--- app/src/main/java/com/example/przemek/to_atrakcja/activities/AdministratorActivity.java
+++ app/src/main/java/com/example/przemek/to_atrakcja/activities/AdministratorActivity.java
@@ -1,8 +1,11 @@
 package com.example.przemek.to_atrakcja.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import com.example.przemek.to_atrakcja.R;
 
 public class AdministratorActivity extends Activity
@@ -17,6 +20,14 @@ public class AdministratorActivity extends Activity
     @Override
     public void onBackPressed()
     {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void Logout (View view)
+    {
+        SharedPreferences sharedPreferences = this.getSharedPreferences("DATA", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("Zalogowano?","nie").apply();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
