7bfab767c10b3f0d922b48a5f3fd5ce0bcef64f8
Przemysław Pajestkaw <przemek1793@gmail.com>
2018-01-04 14:09:05 +0100

    Dodawanie map

--- app/src/main/java/com/example/przemek/to_atrakcja/activities/AddMaps.java
+++ app/src/main/java/com/example/przemek/to_atrakcja/activities/AddMaps.java
@@ -2,12 +2,24 @@ package com.example.przemek.to_atrakcja.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import com.example.przemek.to_atrakcja.R;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class AddMaps extends Activity
 {
+    private static String URL_check_login = "http://192.168.0.13/add_map.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -24,6 +36,98 @@ public class AddMaps extends Activity
 
     public void TryToAddMap (View view)
     {
-  //      new LogIn().execute();
+        new AddMap().execute();
+    }
+
+    class AddMap extends AsyncTask<String, String, String> {
+
+        JSONObject jsonResponse;
+        String response;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Button TryToLogButton=(Button)findViewById(R.id.TryToAddMapButton);
+            TryToLogButton.setText("Dodawanie mapy");
+        }
+
+        protected String doInBackground(String... args) {
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+            OutputStream os = null;
+            InputStream is = null;
+            try {
+                String message;
+                EditText InputMapName=findViewById(R.id.InputMapName);
+                EditText InputMapURL=findViewById(R.id.InputMapURL);
+
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("Name", InputMapName.getText().toString());
+                jsonObject.put("MapURL", InputMapURL.getText().toString());
+                message = jsonObject.toString();
+
+                url=new URL(URL_check_login);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(15000);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+                urlConnection.setFixedLengthStreamingMode(message.getBytes().length);
+
+                urlConnection.connect();
+
+                OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+
+                os = new BufferedOutputStream(urlConnection.getOutputStream());
+                os.write(message.getBytes("UTF-8"));
+
+                os.flush();
+
+                urlConnection.getResponseMessage();
+                urlConnection.getResponseCode();
+
+                InputStreamReader aa= new InputStreamReader((urlConnection.getInputStream()));
+                BufferedReader br = new BufferedReader(aa);
+
+                String line;
+                StringBuilder sb = new StringBuilder();
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                response=sb.toString();
+                os.close();
+                br.close();
+
+                urlConnection.disconnect();
+
+                jsonResponse = new JSONObject(response) ;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            try {
+                TextView DatabaseResponse = (TextView) findViewById(R.id.DatabaseResponseAddMap);
+                DatabaseResponse.setText(jsonResponse.getString("message"));
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+            Button TryToLogButton=(Button)findViewById(R.id.TryToAddMapButton);
+            TryToLogButton.setText("Dodaj mapę");
+        }
+
     }
 }
